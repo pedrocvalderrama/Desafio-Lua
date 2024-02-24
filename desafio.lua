dd73b50f89cecad3af01eb0a436be3d534da5c0a
pedrocvalderrama <superbeiobros@gmail.com>
2024-02-24 09:39:08 -0300

    Update desafio.lua

--- desafio.lua
+++ desafio.lua
@@ -1,21 +1,34 @@
 os.execute("chcp 65001")
+os.execute("cls")
 
--- Criatura
+
+
+-- Description
 local monsterName = "Flamme the Fire Mage"
 local description = "Um famoso mago que domina todas as artes de magia de fogo"
+
+-- Characteristics
 local emoji = "🔥"
 local sound = "You will be consumed by fire"
 local favoriteTime = "Noturno"
 local item = "Grimório de Flamme"
 
-
--- Atributos
+-- Attributes
 local attackAttribute = 3
 local defenseAttribute = 2
 local lifeAttribute = 6
 local speedAttribute = 4
 local inteligenceAttribute = 10
 
+-- History
+local monsterOrigin = "Um elfo criado em Einshely, um território na extremidade norte do planeta, habito apenas por elfos."
+local monsterPast = "Foi discípulo do grande mago Magma e depois de treinar 100 anos, fugiu de seu país."
+local monsterCurrent = "Hoje vive em seu castelo, atuando como mago na cidade de Eidan."
+
+local function printBreak()
+    print("|")
+    
+end
 
 local function getProgressBar(attribute)
 
@@ -38,23 +51,54 @@ local function getProgressBar(attribute)
     
 end
 
+local function printEdge()
+    print("|===================================================================================================================")
+    printBreak()
+end
+
+local function printAttributes()
+    print("| Atributos")
+    print("|       Ataque: " .. getProgressBar(attackAttribute))
+    print("|       Defesa: " .. getProgressBar(defenseAttribute))
+    print("|         Vida: " .. getProgressBar(lifeAttribute))
+    print("|   Velocidade: " .. getProgressBar(speedAttribute))
+    print("| Inteligência: " .. getProgressBar(inteligenceAttribute))
+    printBreak()
+    
+end
+
+local function printDescription()
+    print("| " .. monsterName)
+    print("| " .. description)  
+    printBreak()
+end
+
+local function printCharacteristics()
+    print("| Item: " .. item)
+    print("| Som: " .. sound)
+    print("| Emoji Favorito: " .. emoji)
+    print("| Horário Favorito: " .. favoriteTime)
+    printBreak()
+    
+end
+
+local function printHistory()
+    print("| História")
+    printBreak()
+    print("| Origem: " .. monsterOrigin)
+    print("| Passado: " .. monsterPast)
+    print("| Atualmente: " .. monsterCurrent)
+    printBreak()
+end
 
 -- Cartão
-print("====================================")
-print("|")
-print("| " .. monsterName)
-print("| " .. description)
-print("|")
-print("| Item: " .. item)
-print("| Som: " .. sound)
-print("| Emoji Favorito: " .. emoji)
-print("| Horário Favorito: " .. favoriteTime)
-print("|")
-print("| Atributos")
-print("|       Ataque: " .. getProgressBar(attackAttribute))
-print("|       Defesa: " .. getProgressBar(defenseAttribute))
-print("|         Vida: " .. getProgressBar(lifeAttribute))
-print("|   Velocidade: " .. getProgressBar(speedAttribute))
-print("| Inteligência: " .. getProgressBar(inteligenceAttribute))
-print("|")
-print("====================================")
+printEdge()
+printDescription()
+printCharacteristics()
+printAttributes()
+printEdge()
+printHistory()
+printEdge()
+
+
+
